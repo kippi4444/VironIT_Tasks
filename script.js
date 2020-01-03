365b2cfd3272fe778013c015f89cccd1145e210a
kippi4444 <kippi1995@gmail.com>
2020-01-03 14:42:54 +0300

    go

--- script.js
+++
@@ -1,8 +0,0 @@
-
- axios.get('https://cors-anywhere.herokuapp.com/http://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
